c2dd4d8fa67356274673d274495a280ae42621c1
Daniel Abella <abelladaniel1@gmail.com>
2021-01-11 09:29:51 +0100

    Translated the english text to swedish.

--- src/i18n/sv/text.mjs
+++ src/i18n/sv/text.mjs
@@ -1,3 +1,20 @@
 export default {
-  test: 'TEST'
+  studyStatusUpdate: {
+    studyAcceptedTitle: 'Verifiering av accepterande av studie { studyTitle }',
+    studyAcceptedThanks: 'Tack för att du accepterar att deltaga i studien { studyTitle }.',
+    studyAcceptedConsentedTasks: 'Du har samtyckt till följande:',
+    studyAcceptedNotConsentedTasks: 'Du har inte samtyckt till följande:',
+    studyCompletedTitle: 'Slutförande av studie { studyTitle }',
+    studyCompletedThanks: 'Studien { studyTitle } har nu blivit slutförd. Tack för ditt deltagande.',
+    studyWithdrawnTitle: 'Withdrawal from study { studyTitle }',
+    studyWithdrawnTitle: 'Withdrawal from study { studyTitle }',
+    studyWithdrawnThanks: 'Du har upphävt ditt deltagande i studien { studyTitle }. Tack för din tid.'
+  },
+  account: {
+    passwordRecoveryTitle: 'Mobistudy återställning av lösenord',
+    passwordRecoveryContent: `<p>Du har begärt att återställa ditt lösenord hos Mobistudy.</p>
+    <p>Navigera till <a href={serverlink}>den här hemsidan</a> för att återställa lösenordet.</p>
+    <p>Eller använd följande kod om nödvändigt: {token}</p>
+    <p>Den här koden upphör att gälla om 24 timmar.</p>`
+  }
 }
