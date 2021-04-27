14b7311cc686370af752bfbcd9954192660316c6
dariosalvi78@gmail.com <BuggyBuggy78>
2021-04-27 20:25:57 +0200

    added getLanguageFromAcceptedList() to i18n

--- src/i18n/i18n.mjs
+++ src/i18n/i18n.mjs
@@ -2,19 +2,29 @@ import entext from './en/text.mjs'
 import svtext from './sv/text.mjs'
 
 // extracts a property using a string, see https://stackoverflow.com/a/6491621/1097607
-let byString = function(o, s) {
-    s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    s = s.replace(/^\./, '');           // strip a leading dot
-    var a = s.split('.');
-    for (var i = 0, n = a.length; i < n; ++i) {
-        var k = a[i];
-        if (k in o) {
-            o = o[k];
-        } else {
-            return;
-        }
+let byString = function (o, s) {
+  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+  s = s.replace(/^\./, '');           // strip a leading dot
+  var a = s.split('.');
+  for (var i = 0, n = a.length; i < n; ++i) {
+    var k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return;
     }
-    return o;
+  }
+  return o;
+}
+
+
+export function getLanguageFromAcceptedList (list) {
+  if (!list) return 'en'
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].startsWith('en')) return 'en'
+    if (list[i].startsWith('sv')) return 'sv'
+  }
+  return 'en'
 }
 
 // API inspired by https://kazupon.github.io/vue-i18n/introduction.html
@@ -24,7 +34,7 @@ export default {
     en: entext,
     sv: svtext
   },
-  t: function(id, args) {
+  t: function (id, args) {
     let text = byString(this.text[this.locale], id)
     if (!text) return undefined
     for (const token in args) {
