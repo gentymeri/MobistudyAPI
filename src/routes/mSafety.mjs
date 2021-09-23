c5224393285ae7b4de01f57bb5faa37e93486427
Dario Salvi <dariosalvi78@gmail.com>
2021-09-23 11:39:34 +0200

    always send a reply when saving files

--- src/routes/mSafety.mjs
+++ src/routes/mSafety.mjs
@@ -5,6 +5,7 @@
 
 import express from 'express'
 import { open } from 'fs/promises'
+import { applogger } from '../services/logger.mjs'
 
 const router = express.Router()
 const UPLOADSDIR = 'tasksuploads'
@@ -14,15 +15,20 @@ export default async function () {
   router.post('/msafety/webhook/', async function (req, res) {
     // temporary implementation, just writes any message to file
     const ts = new Date().getTime()
+    const filename = '/request_' + ts + '.txt'
+    applogger.debug('mSafety storing file ' + filename)
     let filehandle
     try {
-      filehandle = await open(UPLOADSDIR + '/request_' + ts + '.txt', 'w')
+      filehandle = await open(UPLOADSDIR + filename, 'w')
       const text = JSON.stringify({
         headers: req.headers,
         body: req.body
       })
       await filehandle.writeFile(text)
       res.sendStatus(200)
+    } catch (err) {
+      res.sendStatus(500)
+      applogger.error(err, 'cannot save mSafety file' + filename)
     } finally {
       if (filehandle) await filehandle.close()
     }
